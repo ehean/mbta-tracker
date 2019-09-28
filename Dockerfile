098361dc9f0ba455ca662fb4064d6ba34ede0cd4
Erik Heaney <eheaney98@gmail.com>
2019-09-28 13:15:23 -0400

    updated Dockerfile; getNextDeparture returns formatted departure time of default query params

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,15 @@
 FROM ubuntu:latest
 
+RUN echo "set tabstop=2 shiftwidth=2 expandtab" > ~/.vimrc 
+
 RUN apt-get update && apt-get install -y \
-    python3 
-    python3-pip
-    python3-requests
-    python3-boto3 
-    python-flask
-    vim
+    python3.7 \
+    python3-pip \
+    python3-requests \
+    python3-flask \
+    vim 
+#    python3-boto3 \
+
+COPY getNextDeparture.py /apps/getNextDeparture.py
 
 ENTRYPOINT tail -f /dev/null
--- getNextDeparture.py
+++ getNextDeparture.py
@@ -1,11 +1,38 @@
+import pprint
 import requests
+import urllib.parse
+import datetime
 
-API_KEY="a45acc39ad4b2cb73936d6fd8f047a"
+API_KEY="43a45acc39ad4b2cb73936d6fd8f047a"
 MBTA_API_ENDPOINT="https://api-v3.mbta.com/"
 
 firstPath = "predictions/?filter[stop]=North+Station&filter[route]=CR-Lowell&filter[direction_id]=0&include=schedule"
 firstRequest= MBTA_API_ENDPOINT + firstPath
+firstRequestHeader={
+    "X-API-Key": API_KEY
+}    
+northStation=urllib.parse.unquote_plus("North+Station")
+firstRequestParams={
+    "filter[stop]": northStation,
+    "filter[route]": "CR-Lowell",
+    "filter[direction_id]": "0",
+    "include": "schedule",
+}
 
-firstResponse = requests.get(firstRequest)
+firstResponse = requests.get(
+    MBTA_API_ENDPOINT + "predictions/",
+    headers=firstRequestHeader,
+    params=firstRequestParams
+)
 
-print(firstResponse.content)
+# firstResponse = requests.get(MBTA_API_ENDPOINT + firstPath)
+
+print(firstResponse.status_code)
+#print(firstResponse.headers)
+#print(firstResponse.request.url)
+
+#pprint.pprint(firstResponse.json())
+departureTime = firstResponse.json()["included"][0]["attributes"]["departure_time"]
+departureTime = datetime.datetime.fromisoformat(departureTime)
+departureTime = departureTime.time()
+print(departureTime.strftime("%I:%M %p"))
