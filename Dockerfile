0ce61e0a6874d5a8d89d38fb9fed0b907506a3c4
Erik Heaney <eheaney98@gmail.com>
2019-09-28 09:36:21 -0400

    added new packages to Dockerfile; created quick test script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,11 @@
 FROM ubuntu:latest
 
-RUN apt-get update && apt-get install -y python3 python3-boto3 python-flask
+RUN apt-get update && apt-get install -y \
+    python3 
+    python3-pip
+    python3-requests
+    python3-boto3 
+    python-flask
+    vim
 
 ENTRYPOINT tail -f /dev/null
---
+++ getNextDeparture.py
@@ -0,0 +1,11 @@
+import requests
+
+API_KEY="a45acc39ad4b2cb73936d6fd8f047a"
+MBTA_API_ENDPOINT="https://api-v3.mbta.com/"
+
+firstPath = "predictions/?filter[stop]=North+Station&filter[route]=CR-Lowell&filter[direction_id]=0&include=schedule"
+firstRequest= MBTA_API_ENDPOINT + firstPath
+
+firstResponse = requests.get(firstRequest)
+
+print(firstResponse.content)
